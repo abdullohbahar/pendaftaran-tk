1caa0684ec7e68f159bb5e4dedba88a1d754f2e6
Baharudin <abdullohbahar@gmail.com>
2023-11-02 20:14:43 +0700

    create landing page

---
+++ application/views/landing/index.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>TK ABA NGABEAN 1</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/landing/img/favicon.png" rel="icon">
+    <link href="assets/landing/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/landing/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/landing/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/landing/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/landing/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/landing/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Variables CSS Files. Uncomment your preferred color scheme -->
+    <link href="assets/landing/css/variables.css" rel="stylesheet">
+    <!-- <link href="assets/landing/css/variables-blue.css" rel="stylesheet"> -->
+    <!-- <link href="assets/landing/css/variables-green.css" rel="stylesheet"> -->
+    <!-- <link href="assets/landing/css/variables-orange.css" rel="stylesheet"> -->
+    <!-- <link href="assets/landing/css/variables-purple.css" rel="stylesheet"> -->
+    <!-- <link href="assets/landing/css/variables-red.css" rel="stylesheet"> -->
+    <!-- <link href="assets/landing/css/variables-pink.css" rel="stylesheet"> -->
+
+    <!-- Template Main CSS File -->
+    <link href="assets/landing/css/main.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: HeroBiz
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top" data-scrollto-offset="0">
+        <div class="container-fluid d-flex align-items-center justify-content-between">
+
+            <a href="#" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+                <!-- Uncomment the line below if you also wish to use an image logo -->
+                <!-- <img src="assets/landing/img/logo.png" alt=""> -->
+                <h1><b>TK ABA NGABEAN 1</b></h1>
+            </a>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto" href="#header">Home</a></li>
+                    <li><a class="nav-link scrollto" href="#visi">Visi & Misi</a></li>
+                    <li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
+                    <li><a class="nav-link scrollto" href="#teacher">Guru</a></li>
+                    <li><a class="nav-link scrollto" href="#registration">Pendaftaran</a></li>
+                    <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle d-none"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <section id="hero-fullscreen" class="hero-fullscreen d-flex align-items-center">
+        <div class="container d-flex flex-column align-items-center position-relative" data-aos="zoom-out">
+            <h2>Selamat Datang di <span>TK ABA NGABEAN 1</span></h2>
+            <!-- <p>Et voluptate esse accusantium accusamus natus reiciendis quidem voluptates similique aut.</p> -->
+            <div class="d-flex">
+                <a href="#about" class="btn-get-started scrollto">Lakukan Pendaftaran Untuk Putra Putri Anda</a>
+                <!-- <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> -->
+            </div>
+        </div>
+    </section>
+
+    <main id="main">
+        <!-- ======= About Section ======= -->
+        <section id="visi" class="visi">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-header">
+                    <h2>Visi Dan Misi </h2>
+                </div>
+
+                <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-6 text-center">
+                        <h1 class="text-center">Visi</h1>
+                        <p>Hola 1</p>
+                        <p>Hola 2</p>
+                        <p>Hola 3</p>
+                    </div>
+                    <div class="col-6 text-center">
+                        <h1 class="text-center">Misi</h1>
+                        <p>Halo 1</p>
+                        <p>Halo 2</p>
+                        <p>Halo 3</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End About Section -->
+
+        <section id="about" class="about">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-header">
+                    <h2>Tentang Kami</h2>
+                    <p>Architecto nobis eos vel nam quidem vitae temporibus voluptates qui hic deserunt iusto omnis nam voluptas asperiores sequi tenetur dolores incidunt enim voluptatem magnam cumque fuga.</p>
+                </div>
+
+                <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
+
+                    <div class="col-lg-5">
+                        <div class="about-img">
+                            <img src="assets/landing/img/tk2.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-7">
+                        kami adalah Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex tempore quod reprehenderit labore quae consectetur debitis perferendis dolorem sequi, eos obcaecati quia repellendus, quos dicta dolores itaque esse ut reiciendis.
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End About Section -->
+
+        <!-- ======= Team Section ======= -->
+        <section id="teacher" class="teacher">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-header">
+                    <h2>Guru Kami</h2>
+                    <p>Architecto nobis eos vel nam quidem vitae temporibus voluptates qui hic deserunt iusto omnis nam voluptas asperiores sequi tenetur dolores incidunt enim voluptatem magnam cumque fuga.</p>
+                </div>
+
+                <div class="row gy-5">
+
+                    <div class="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="200">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/landing/img/team/team-1.jpg" class="img-fluid" alt="">
+                            </div>
+                            <div class="member-info">
+
+                                <h4>Walter White</h4>
+                                <span>Chief Executive Officer</span>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                    <div class="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="400">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/landing/img/team/team-2.jpg" class="img-fluid" alt="">
+                            </div>
+                            <div class="member-info">
+
+                                <h4>Sarah Jhonson</h4>
+                                <span>Product Manager</span>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                    <div class="col-xl-4 col-md-6 d-flex" data-aos="zoom-in" data-aos-delay="600">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/landing/img/team/team-3.jpg" class="img-fluid" alt="">
+                            </div>
+                            <div class="member-info">
+
+                                <h4>William Anderson</h4>
+                                <span>CTO</span>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                </div>
+
+            </div>
+        </section><!-- End Team Section -->
+
+        <section id="registration" class="registration">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-header">
+                    <h2>Daftarkan Putra Putri Anda Disini </h2>
+                </div>
+
+                <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
+                    <div class="text-center">
+                        <a href="" class="btn btn-success">Klik Disini Untuk Melakukan Pendaftaran Untuk Putra Putri Anda</a>
+                        <!-- <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> -->
+                    </div>
+                </div>
+
+            </div>
+        </section><!-- End About Section -->
+
+
+        <!-- ======= Contact Section ======= -->
+        <section id="contact" class="contact">
+            <div class="container">
+
+                <div class="section-header">
+                    <h2>Hubungi Kami</h2>
+                    <p>Ea vitae aspernatur deserunt voluptatem impedit deserunt magnam occaecati dssumenda quas ut ad dolores adipisci aliquam.</p>
+                </div>
+
+            </div>
+
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15815.367999254819!2d110.288195!3d-7.7000992!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7af5cbcf17a6ff%3A0x7e95cf140760f86f!2sTK%20ABA%20Ngabean%201!5e0!3m2!1sid!2sid!4v1698930496096!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div><!-- End Google Maps -->
+
+            <div class="container">
+
+                <div class="row gy-5 gx-lg-5">
+
+                    <div class="col-lg-12">
+
+                        <div class="info">
+                            <h3>Hubungi Kami</h3>
+                            <p>Et id eius voluptates atque nihil voluptatem enim in tempore minima sit ad mollitia commodi minus.</p>
+
+                            <div class="info-item d-flex">
+                                <i class="bi bi-geo-alt flex-shrink-0"></i>
+                                <div>
+                                    <h4>Location:</h4>
+                                    <p>A108 Adam Street, New York, NY 535022</p>
+                                </div>
+                            </div><!-- End Info Item -->
+
+                            <div class="info-item d-flex">
+                                <i class="bi bi-envelope flex-shrink-0"></i>
+                                <div>
+                                    <h4>Email:</h4>
+                                    <p>info@example.com</p>
+                                </div>
+                            </div><!-- End Info Item -->
+
+                            <div class="info-item d-flex">
+                                <i class="bi bi-phone flex-shrink-0"></i>
+                                <div>
+                                    <h4>Call:</h4>
+                                    <p>+1 5589 55488 55</p>
+                                </div>
+                            </div><!-- End Info Item -->
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Contact Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+
+        <div class="footer-content">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-info">
+                            <h3>TK ABA NGABEAN 1</h3>
+                            <p>
+                                A108 Adam Street <br>
+                                NY 535022, USA<br><br>
+                                <strong>Phone:</strong> +1 5589 55488 55<br>
+                                <strong>Email:</strong> info@example.com<br>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </footer><!-- End Footer -->
+
+    <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <div id="preloader"></div>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/landing/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/landing/vendor/aos/aos.js"></script>
+    <script src="assets/landing/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/landing/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/landing/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/landing/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/landing/js/main.js"></script>
+
+</body>
+
+</html>
